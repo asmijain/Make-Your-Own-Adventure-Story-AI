2adc2237de2e01a7bdbd15677f3733ba0e6feabd
Asmi Jain <asmijain3014@gmail.com>
2026-06-16 13:52:15 +0530

    fix: requirements.txt encoding and vite.config function signature

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig((command, mode) => {
+export default defineConfig(({command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "")
 
   console.log(env.VITE_DEBUG)
